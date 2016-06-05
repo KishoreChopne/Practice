0880926920725b283e9075262f886daa75a7dbbf
Kishore Chopne <kishor@Kishores-MacBook-Pro.local>
2016-06-05 22:31:23 +0530

    practice problem

---
+++ num_of_sum_prod.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+if __name__ == "__main__":
+	seed  = int(raw_input("Enter a number to calculate result:"))
+	operation = raw_input("Enter an operation to perform:")
+
+	result = 0
+
+	if operation == '*':
+		result = 1 
+
+	for i in range(result,seed):
+		if operation == '*':
+			result*= i
+		elif operation == '+':
+			result+= i
+
+	print "Final result %s" %result
+		
