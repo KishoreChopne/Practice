a0d4ec49d396fe90ec7d5af9f7046fe5ed747a67
Kishore Chopne <kishor@Kishores-MacBook-Pro.local>
2016-06-05 23:05:29 +0530

    test

--- num_of_sum_prod.py
+++ num_of_sum_prod.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
 	seed  = int(raw_input("Enter a number to calculate result:"))
 	operation = raw_input("Enter an operation to perform:")
 
-	result = 1
+	result = 0
+
+	if operation == '*':
+		result = 1 
 
 	for i in range(result,seed):
 		if operation == '*':
